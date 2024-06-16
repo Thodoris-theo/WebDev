8d9e46f98167856da44264638acb9c8a2954498f
Pipikan <kanakisspy@gmail.com>
2024-06-16 12:02:18 +0300

    BamBam

--- src/api/view_patients.js
+++ src/api/view_patients.js
@@ -2,9 +2,10 @@ const db = require('../db_connect');
 
 const getPatients = (callback) => {
     const query = `
-        SELECT email, username, firstName, lastName, identity_number
-        FROM users
-        WHERE role = 'patient'`;
+        SELECT u.id, u.username, u.email, u.firstName, u.lastName, u.identity_number, p.amka
+        FROM users u
+        LEFT JOIN patients p ON u.id = p.user_id
+        WHERE u.role = 'patient'`;
 
     db.query(query, (err, result) => {
         if (err) {
